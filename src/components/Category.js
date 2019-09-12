76d3f44da692fc39b00cc7673051d4190738b917
Meleesha <mayola.dsz@gmail.com>
2019-09-12 14:10:13 +0100

    display questions in the category component

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,17 +4,29 @@ import { connect } from 'react-redux';
 import { pickCategory } from '../actions/index';
 
 class Category extends Component {
+    constructor() {
+        super();
+        this.state = {
+            clues: []
+        }
+    }
+
     componentDidMount() {
         fetch(`http://jservice.io/api/clues?category=${this.props.category.id}`)
             .then(response => response.json())
-            .then(json => console.log(json))
+            .then(json => this.setState({ clues: json }))
     }
+
     render() {
-        console.log(this.props)
         return (
             <div>
                 <Link to="/" className="link-home"><h4>Home</h4></Link>
                 <h2>{this.props.category.title}</h2>
+                {this.state.clues.map(clue =>
+                    <div key={clue.id}>
+                        {clue.question}
+                    </div>
+                )}
             </div>
         )
     }
