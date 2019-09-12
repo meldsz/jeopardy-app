331bb03205abfce697f41f0c0110ad7089430dda
Meleesha <mayola.dsz@gmail.com>
2019-09-12 17:17:49 +0100

    mock data fixtures for testing

---
+++ src/data/fixtures.js
@@ -0,0 +1,14 @@
+export const categories = [
+    { id: 0, title: 'category one' },
+    { id: 1, title: 'category two' },
+    { id: 2, title: 'category three' }
+];
+
+export const clue = {
+    id: 0,
+    value: 100,
+    question: 'test question',
+    answer: 'test answer'
+}
+
+export const category = { id: 0, title: 'test title' };
